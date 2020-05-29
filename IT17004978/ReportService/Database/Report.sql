81c7e5b6fe478a2b56fdbcb5055e204b5ab2fdb5
thisura1997 <47887864+thisura1997@users.noreply.github.com>
2020-05-29 21:22:08 +0530

    Add files via upload

---
+++ IT17004978/ReportService/Database/Report.sql
@@ -0,0 +1,12 @@
+
+
+create table Report(
+	id  int(3) NOT NULL AUTO_INCREMENT,
+	name varchar(120) NOT NULL,
+	dep varchar(120) NOT NULL,
+	address varchar(220) NOT NULL,
+	num varchar(120),
+	
+	PRIMARY KEY (id)
+);
+
